8be0b55ee32f049de5d1d7829dcd32ceb0a84ed4
Rafael Blanco Aranda <rblancar@emeal.nttdata.com>
2023-05-22 01:57:30 +0200

    Header y Footer added to Admin Profile

--- src/profiles/AdminProfile.js
+++ src/profiles/AdminProfile.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom'
+import Navbar from '../layout/NavbarAdmin';
+import Footer from '../layout/FooterAdmin';
 
 
 const AdminProfile = () => {
     const [userData, setUserData] = useState(null);
-  
+
     let navigate = useNavigate()
 
     function handleToken() {
@@ -19,68 +21,72 @@ const AdminProfile = () => {
 
 
     useEffect(() => {
-      // Lógica para realizar la petición HTTP y obtener los datos del usuario
-      const fetchUserData = async () => {
-        try {
-          const token = localStorage.getItem('token');
-          const username= localStorage.getItem('username');
-          const response = await axios.get(`http://localhost:8080/replica/v1/users/username/${username}`, {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          });
+        // Lógica para realizar la petición HTTP y obtener los datos del usuario
+        const fetchUserData = async () => {
+            try {
+                const token = localStorage.getItem('token');
+                const username = localStorage.getItem('username');
+                const response = await axios.get(`http://localhost:8080/replica/v1/users/username/${username}`, {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
 
-          console.log(response);
-  
-          // Almacena los datos del usuario en el estado
-          setUserData(response.data);
+                console.log(response);
 
-        } catch (error) {
-          console.log('Error al obtener los datos del usuario', error);
-        }
-      };
-  
-      fetchUserData();
+                // Almacena los datos del usuario en el estado
+                setUserData(response.data);
+
+            } catch (error) {
+                console.log('Error al obtener los datos del usuario', error);
+            }
+        };
+
+        fetchUserData();
     }, []);
     return (
-        <div className="container">
-            <div className='row mb-5'>
-                <div className='col-md-6 offset-md-3 border rounded p-4 mt-5 shadow'>
-                    <h2 className='text-center m-4'>Perfil de usuario</h2>
-                    <div className='card'>
-                        <div className='card-header'>
-                            <b> Nº de Usuario:</b>
-                            {userData?.data?.id}
-                            <ul className='list-group list-group-flush'>
-                                <li className='list-group-item'>
-                                    <b>Nombre de Usuario: </b>
-                                    {userData?.data?.username}
-                                </li>
-                                <li className='list-group-item'>
-                                    <b>Correo Electrónico </b>
-                                    {userData?.data?.email}
-                                </li>
-                                <li className='list-group-item'>
-                                    <b>Contraseña: </b>
-                                    {userData?.data?.password}
-                                </li>
-                                <li className='list-group-item'>
-                                    <b>Rol: </b>
-                                    {userData?.data?.roles[0].name}
-                                </li>
-                            </ul>
+        <div>
+            <Navbar/>
+            <div className="container">
+                <div className='row mb-5'>
+                    <div className='col-md-6 offset-md-3 border rounded p-4 mt-5 shadow'>
+                        <h2 className='text-center m-4'>Perfil de usuario</h2>
+                        <div className='card'>
+                            <div className='card-header'>
+                                <b> Nº de Usuario:</b>
+                                {userData?.data?.id}
+                                <ul className='list-group list-group-flush'>
+                                    <li className='list-group-item'>
+                                        <b>Nombre de Usuario: </b>
+                                        {userData?.data?.username}
+                                    </li>
+                                    <li className='list-group-item'>
+                                        <b>Correo Electrónico </b>
+                                        {userData?.data?.email}
+                                    </li>
+                                    <li className='list-group-item'>
+                                        <b>Contraseña: </b>
+                                        {userData?.data?.password}
+                                    </li>
+                                    <li className='list-group-item'>
+                                        <b>Rol: </b>
+                                        {userData?.data?.roles[0].name}
+                                    </li>
+                                </ul>
+                            </div>
+
+                            <button className="btn btn-danger mx-2" onClick={handleToken}>Cerrar Sesión</button>
+
+
                         </div>
-                        
-                        <button className="btn btn-danger mx-2" onClick={handleToken}>Cerrar Sesión</button>
-                        
-
+                        <Link className="btn btn-primary my-2" to={"/home"}>Volver</Link>
                     </div>
-                    <Link className="btn btn-primary my-2" to={"/home"}>Volver</Link>
-                </div>
 
+                </div>
             </div>
+            <Footer/>
         </div>
-   );
+    );
 };
 
 export default AdminProfile;
